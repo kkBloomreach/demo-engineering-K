2b5a7410cc477097def6dfee715d9cfc860d599f
kiran.kundargi <kiran.kundargi@bloomreach.com>
2026-06-10 15:32:28 -0700

    changed docker file to use version number

--- dailyindexer/dockerize.sh
+++ dailyindexer/dockerize.sh
@@ -2,10 +2,10 @@
 
 # build docker image so it will run in linux/amd64 environment
 # GCP vm has debian / amd64
-docker buildx build --platform linux/amd64  -t reindexer-docker-amd64 .
+docker buildx build --platform linux/amd64  -t reindexer-3.2-docker-amd64 .
 
 # create a tar for this image
-docker save -o ~/tmp/misc/reindexer-docker-amd64.tar reindexer-docker-amd64:latest
+docker save -o ~/tmp/misc/reindexer-3.2-docker-amd64.tar reindexer-3.2-docker-amd64:latest
 
 # upload the tar to GCP then load and run it
 # ON GCP-VM
