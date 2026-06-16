58a9495f7b2208b9a76f14edf30c9a5afaf795c2
kiran.kundargi <kiran.kundargi@bloomreach.com>
2026-06-16 15:50:38 -0700

    Moved fake_purchase and openai_image_generator out of this folder

--- fake_pastpurchase/pacifichome/english/verify_pastpurchase.sh
+++
@@ -1,7 +0,0 @@
-USER_ID=1112223334445
-
-# PACIFIC HOME
-# past purchase, user_id = 1112223334445
-curl -X GET "http://pathways.dxpapi.com/api/v2/widgets/personalized/1jwmk29z?account_id=6413&domain_key=pacifichome&_br_uid_2=uid%3Dsample%3Av%3D11.8%3Ats%3D1585222161176%3Ahc%3D30&url=pacific.bloomreach.com/home&query=*&user_id=$USER_ID&ref_url=pacific.bloomreach.com/home&request_id=123456789" -H "accept: application/json" -H "auth-key: bcvpynhij980k0y1"
-
-
